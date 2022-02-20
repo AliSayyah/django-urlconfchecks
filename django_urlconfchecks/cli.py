2adffe5f8558a1e1f2e0f096771a4228dbc5215f
alisayyah <ali.sayyah2@gmail.com>
2022-02-21 01:16:16 +0330

    a better version

--- django_urlconfchecks/cli.py
+++ django_urlconfchecks/cli.py
@@ -1,4 +1,5 @@
 """CLI module for django_urlconfchecks."""
+import importlib
 import os
 from pathlib import Path
 from typing import Optional
@@ -16,19 +17,29 @@ import typer
 app = typer.Typer()
 
 
-def find_settings_module() -> Optional[str]:
+def setup_django():
     """Find the settings.py in current directory and subdirectories.
 
     Returns:
         str: the path to the settings.py
     """
     current_dir = Path(os.getcwd())
-    for path in current_dir.glob('**/settings.py'):
-        return str(path.relative_to(current_dir)).replace("/", ".").replace(".py", "")
-    return None
+    for path in current_dir.glob('**/manage.py'):
+        try:
+            manage = importlib.import_module(str(path.relative_to(current_dir)).replace(".py", "").replace("/", "."))
+            main = getattr(manage, 'main')
+            main()
+            django.setup()
+            return
+        except ImportError:
+            continue
+
+    typer.secho("Could not find manage.py in current directory or subdirectories.\n"
+                "Make sure you are in the project root directory.", fg=typer.colors.RED)
+    raise typer.Exit(1)
 
 
-SETTINGS = find_settings_module()
+setup_django()
 
 
 def version_callback(value: bool):
@@ -41,17 +52,11 @@ def version_callback(value: bool):
 # noinspection PyUnusedLocal
 @app.command()
 def run(
-    settings: str = typer.Option(
-        default=SETTINGS,
-        help="The settings module to use.\nurlconfchecks will try to find the settings automatically.",
-        show_default=True,
-    ),
     version: Optional[bool] = typer.Option(None, "--version", callback=version_callback),
-) -> int:
+) -> None:
     """Check all URLConfs for errors.
 
     Args:
-        settings (str): The settings module to use.
         version (bool, optional): Show version. Defaults to None.
 
     Returns:
@@ -59,22 +64,7 @@ def run(
     """
     if not django.VERSION[0:2] >= (3, 2):
         typer.secho("Django version 3.2 or higher is required.", fg=typer.colors.RED)
-        return 1
-
-    if SETTINGS is None:
-        typer.secho("settings.py not found.", fg=typer.colors.RED)
-        typer.secho("Please specify the settings module path with the --settings option.", fg=typer.colors.YELLOW)
-        return 1
-
-    os.environ['DJANGO_SETTINGS_MODULE'] = settings
-    print(os.environ['DJANGO_SETTINGS_MODULE'] + " is used.")
-    django.setup()
-
-    typer.secho(
-        f"Using settings module: {settings}\nIf this module is not correct,"
-        f" please specify path with --settings option.",
-        fg=typer.colors.GREEN,
-    )
+        typer.Exit(1)
 
     from django_urlconfchecks.check import check_url_signatures
 
@@ -86,7 +76,6 @@ def run(
                 typer.secho(f"{error}", fg=typer.colors.RED)
             else:
                 typer.secho(f"{error}", fg=typer.colors.YELLOW)
-        return 1
+        typer.Exit(1)
 
     typer.secho("Done. No errors found.", fg=typer.colors.GREEN)
-    return 0
